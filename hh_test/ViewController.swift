4e07970226fa0e3237fc55f132cbbd00777d23ea
Igor Vedeneev <getmaxx@kilograpp.com>
2018-02-13 21:04:29 +0300

    Initial Commit

---
+++ hh_test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hh_test
+//
+//  Created by Igor Vedeneev on 13.02.18.
+//  Copyright © 2018 Igor Vedeneev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
